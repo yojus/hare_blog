c05cea647e7809a5257f81c0decec3a452c145a0
velmouth-star <shanpuyangshu@gmail.com>
2022-09-24 11:26:22 +0900

    Breezeのviewを修正

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use GuzzleHttp\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])
+    ->name('root');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+// middleware('auth')を追加することで、認証していなければアクセスできないように制御することが可能
+Route::resource('posts', PostController::class)
+    ->only(['create', 'store', 'edit', 'update', 'destroy'])
+    ->Middleware('auth');
+
+// 非ログインでもshow,indexは閲覧可能
+Route::resource('posts', PostController::class)
+    ->only(['show', 'index']);
+
 require __DIR__.'/auth.php';
+
+// exceptもしくはonlyが使える
